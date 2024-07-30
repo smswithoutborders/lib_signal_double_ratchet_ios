b9e3878f9db9dccc6bbb33e421fb94fcb83555a9
sherlock <wisdomnji@gmail.com>
2024-07-30 20:04:14 +0100

    update: states can be serialized and headers pass all test

--- smswithoutborders_libsig_doubleratchet/Ratchets/states.swift
+++ smswithoutborders_libsig_doubleratchet/Ratchets/states.swift
@@ -8,53 +8,102 @@
 import Foundation
 import CryptoKit
 
-class States {
+extension Array where Element == UInt8 {
+    func toBase64() -> String {
+        return Data(self).base64EncodedString()
+    }
+    
+    static func fromBase64(_ base64String: String) -> [UInt8]? {
+        guard let data = Data(base64Encoded: base64String) else { return nil }
+        return [UInt8](data)
+    }
+}
+
+class States: Equatable {
+    
     var DHs: Curve25519.KeyAgreement.PrivateKey? = nil
     var DHr: Curve25519.KeyAgreement.PrivateKey.PublicKey? = nil
-
+    
     var RK: [UInt8]? = nil
     var CKs: [UInt8]? = nil
     var CKr: [UInt8]? = nil
-
+    
     var Ns = 0
     var Nr = 0
-
+    
     var PN = 0
-
-    var MKSKIPPED: [[UInt8]: Int]?
+    
+    var MKSKIPPED: [([[UInt8]: Int]): [UInt8]]?
     
     func serialized() -> Data {
-        let privateKey = self.DHs!.rawRepresentation
-        let publicKey = self.DHs!.publicKey.rawRepresentation
+        let privateKey = self.DHs!.rawRepresentation.base64EncodedString()
         
-        var lenRk = RK!.count
-        var lenCks = CKs!.count
-        var lenCkr = CKr!.count
+        let publicKey = self.DHs!.publicKey.rawRepresentation.base64EncodedString()
         
         var data = Data()
-        data.append(Data(bytes: &lenRk,
-                         count: MemoryLayout.size(ofValue: lenRk)))
-        data.append(Data(bytes: &lenCks,
-                         count: MemoryLayout.size(ofValue: lenCks)))
-        data.append(Data(bytes: &lenCkr,
-                         count: MemoryLayout.size(ofValue: lenCkr)))
-        
-        data.append(Data(bytes: &Ns,
-                         count: MemoryLayout.size(ofValue: Ns)))
-        data.append(Data(bytes: &Nr,
-                         count: MemoryLayout.size(ofValue: Nr)))
-        data.append(Data(bytes: &PN,
-                         count: MemoryLayout.size(ofValue: PN)))
+        data.append(String(Ns).data(using: .utf8)!)
+        data.append(" ".data(using: .utf8)!)
+        data.append(String(Nr).data(using: .utf8)!)
+        data.append(" ".data(using: .utf8)!)
+        data.append(String(PN).data(using: .utf8)!)
+        data.append(" ".data(using: .utf8)!)
+        data.append(RK!.toBase64().data(using: .utf8)!)
+        data.append(" ".data(using: .utf8)!)
+        data.append(CKs!.toBase64().data(using: .utf8)!)
+        data.append(" ".data(using: .utf8)!)
+        data.append(CKr!.toBase64().data(using: .utf8)!)
+        data.append(" ".data(using: .utf8)!)
+        data.append(privateKey.data(using: .utf8)!)
+        data.append(" ".data(using: .utf8)!)
+        data.append(publicKey.data(using: .utf8)!)
 
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(MKSKIPPED) {
+            if let jsonString = String(data: encoded, encoding: .utf8) {
+                data.append((jsonString.data(using: .utf8)?.base64EncodedString().data(using: .utf8))!)
+            }
+        }
+        
         return data
     }
     
-    static func deserialize(data: Data) -> States {
-        let int1 = data[0]
-        let int2 = data[1]
-        let int3 = data[2]
-        let int4 = data[3]
-            
-        return (int1, int2, int3, int4)
+    static func deserialize(data: Data) throws -> States? {
+        let state = States()
+        
+        guard let string = String(data: data, encoding: .utf8) else { return nil }
+        let components = string.split(separator: " ")
+        
+        guard components.count >= 9 else { return nil }
+        
+        state.Ns = Int(components[0])!
+        state.Nr = Int(components[1])!
+        state.PN = Int(components[2])!
+        
+        state.RK = Array<UInt8>.fromBase64(String(components[3]))
+        state.CKs = Array<UInt8>.fromBase64(String(components[4]))
+        state.CKr = Array<UInt8>.fromBase64(String(components[5]))
+        
+        let privateKey = Array<UInt8>.fromBase64(String(components[6]))
+        let publicKey = Array<UInt8>.fromBase64(String(components[7]))
+        state.DHs = try Curve25519.KeyAgreement.PrivateKey.init(rawRepresentation: privateKey!)
+        state.DHr = try Curve25519.KeyAgreement.PublicKey.init(rawRepresentation: publicKey!)
+
+        if let mkSkippedData = Data(base64Encoded: String(components[8])),
+           let decodedDict = try? JSONDecoder().decode([([[UInt8]: Int]): [UInt8]].self, from: mkSkippedData) {
+            state.MKSKIPPED = decodedDict
+        }
+        
+        return state
+    }
+    
+    static func == (lhs: States, rhs: States) -> Bool {
+        return lhs.Ns == rhs.Ns &&
+        lhs.Nr == rhs.Nr &&
+        lhs.PN == rhs.PN &&
+        lhs.RK == rhs.RK &&
+        lhs.CKs == rhs.CKs &&
+        lhs.CKr == rhs.CKr &&
+        lhs.DHs?.rawRepresentation == rhs.DHs?.rawRepresentation &&
+        lhs.DHr?.rawRepresentation == rhs.DHr?.rawRepresentation
     }
 }
--- smswithoutborders_libsig_doubleratchet_Test/smswithoutborders_libsig_doubleratchet_Test.swift
+++ smswithoutborders_libsig_doubleratchet_Test/smswithoutborders_libsig_doubleratchet_Test.swift
@@ -12,12 +12,13 @@ import XCTest
 import CryptoKit
 
 struct smswithoutborders_libsig_doubleratchet_Test {
+    
+    var privateKey = Curve25519.KeyAgreement.PrivateKey()
+    
 
-    @Test func testExample() async throws {
-        let privateKey = Curve25519.KeyAgreement.PrivateKey()
-        let publicKey = privateKey.publicKey
+    @Test func testHeaders() async throws {
         
-        let headers = HEADERS(dhPair: publicKey,
+        let headers = HEADERS(dhPair: privateKey.publicKey,
                       PN: 0,
                       N: 0)
         let serialized = headers.serialize()
@@ -33,6 +34,44 @@ struct smswithoutborders_libsig_doubleratchet_Test {
 
         XCTAssertEqual(serialized, serialized1)
         XCTAssertEqual(headers, headers1)
+    }
+    
+    @Test func testStates() async throws {
+        let d = "Hello world".data(using: .utf8)?.withUnsafeBytes { data in
+            return Array(data)
+        }
+
+        let d1 = "Hello world 1".data(using: .utf8)?.withUnsafeBytes { data in
+            return Array(data)
+        }
+        
+        let rprikey = privateKey.rawRepresentation.withUnsafeBytes { data in
+            return Array(data)
+        }
+        
+        let rpubkey = privateKey.publicKey.rawRepresentation.withUnsafeBytes { data in
+            return Array(data)
+        }
+
+        let states = States()
+        states.DHs = privateKey
+        states.DHr = privateKey.publicKey
+        
+        states.RK = "RK".data(using: .utf8)?.withUnsafeBytes { data in
+            return Array(data)
+        }
+        states.CKs = "CKs".data(using: .utf8)?.withUnsafeBytes { data in
+            return Array(data)
+        }
+        states.CKr = "CKr".data(using: .utf8)?.withUnsafeBytes { data in
+            return Array(data)
+        }
+        states.MKSKIPPED = [([rprikey:0]):d!, ([rpubkey:1]): d!]
+        
+        let sStates = states.serialized()
+        let states1 = try States.deserialize(data: sStates)
+        
+        XCTAssertEqual(states, states1)
     }
 
 }
