c7a542797cf10b49582be83bf3b6d29f986b25b9
sherlock <wisdomnji@gmail.com>
2024-08-01 11:23:15 +0100

    update: fixed issue with double ratchet

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "libsig_doubleratchet_smswithoutborders",
-    platforms: [.macOS(.v14), .iOS(.v13), .macOS(.v12)],
+    platforms: [.macOS(.v14), .iOS(.v13), .watchOS(.v8)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
